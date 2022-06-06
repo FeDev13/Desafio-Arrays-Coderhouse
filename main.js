671e76a458753299f689c2959a225c7c1fc674d1
Federico Robledo <federicorobledo85@gmail.com>
2022-06-06 18:03:20 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+//funcion constructura
+function Producto(id, nombre, precio, stock) {
+  this.id = id;
+  this.nombre = nombre;
+  this.precio = precio;
+  this.stock = stock;
+  //metodo
+  this.valorPrecio = function () {
+    console.log("el producto vale" + this.precio);
+  };
+}
+
+//se instancian los objetos nuevos
+
+const producto1 = new Producto(8745, "crema limpiadora", 123, false);
+console.log(producto1);
+const producto2 = new Producto(8746, "envase retornable", 130, true);
+console.log(producto2);
+const producto3 = new Producto(8747, "mascarilla", 140, true);
+console.log(producto3);
+
+//funcion iteradora sobre un objeto
+
+function hayStock(obj) {
+  while (producto1.nombre === "crema limpiadora" && producto1.id === 8745) {
+    for (const prop in obj) {
+      if (obj.stock === true) {
+        return "en stock";
+      } else {
+        return "sin stock";
+      }
+    }
+  }
+}
+console.log(hayStock(producto1));
