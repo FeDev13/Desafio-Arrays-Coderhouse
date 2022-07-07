44b6eb1dbc44f503b6d2932bf2518734ef183e34
Federico Robledo <federicorobledo85@gmail.com>
2022-07-07 15:55:54 -0300

    se modificaron librerias

--- login.js
+++ login.js
@@ -4,11 +4,20 @@ function entrar() {
   console.log(usuario, pass);
 
   usuario === "fede" && pass === "222"
-    ? (window.location.href = "index.html")
+    ? Swal.fire({
+        icon: "success",
+        title: "Hola!",
+        text: "Bienvenido de vuelta Usuario!",
+        footer: '<a href="">Solucionar el problema?</a>',
+      })(
+        setTimeout(() => {
+          window.location.href = "index.html";
+        }, 2000)
+      )
     : Swal.fire({
         icon: "error",
         title: "Error",
         text: "Los datos ingresados son incorrectos!",
-        footer: '<a href="">Solucionar el problema</a>',
+        footer: '<a href="">Solucionar el problema?</a>',
       });
 }
