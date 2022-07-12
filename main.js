1601fc547b6a973c1cd057dc6c0509b0ba648340
Federico Robledo <federicorobledo85@gmail.com>
2022-07-12 20:48:07 -0300

    metodo fetch para cards y carrito

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const productosLista = document.getElementById("contenedorProductos");
 const productoscarrito = document.querySelector(".carrito-main");
 const sumarCarrito = document.getElementById("carritonav");
-const renderDom = document.getElementById("products-id");
+const renderDom = document.getElementById("div-vacio");
+const avisoDeCompra = document.getElementById("botonFinalizar");
 
 const traerCatalogo = async () => {
   let respuesta = await fetch("./catalogo.json");
@@ -22,7 +23,7 @@ const renderCards = async () => {
             <h5 id="tituloProducto">${Nombre}</h5>
             <p id="descripcionProducto">${Categoria}</p>
             <p id="precioProducto">$${Precio}</p>
-            <button data-id="${Id}" id="mybtn" name="btnComprar" class="btn btn-dark">Comprar</button>
+            <button data-id="${Id}" id="mybtn" name="btnComprar" class="btn btn-dark">Agregar al carrito</button>
             </div>
             </div>
             </div>
@@ -32,7 +33,6 @@ const renderCards = async () => {
 };
 renderCards();
 
-//evento click para agregar producto
 //evento click para agregar producto
 productosLista.addEventListener("click", (e) => {
   if (e.target.id === "mybtn") {
@@ -65,25 +65,25 @@ const recorrerLocalStorage = () => {
     carrito.push(JSON.parse(localStorage.getItem(element)));
   }
   renderCart();
+  console.log(carrito);
 };
 
 const renderCart = () => {
   if (carrito.length > 0) {
-    renderDom.innerHTML = "";
     carrito.forEach((producto) => {
       let { Id, Nombre, Foto, Categoria, Stock, Precio } = producto;
-      renderDom.innerHTML += `<div class="col-12 mb-2 col-md-4 col-sm-4 panel">
-                <div class="card panel1" style="background-color:#BF8085">
-                <div class="card-body">
-                <img id="fotoProducto" src="${Foto}" class="card-img-top">
-                <h5 id="tituloProducto">${Nombre}</h5>
-                <p id="descripcionProducto">${Categoria}</p>
-                <p id="precioProducto">$${Precio}</p>
-                <button data-id="${Id}" id="mybtn" name="btnComprar" class="btn btn-dark">Comprar</button>
-                </div>
-                </div>
-                </div>
-                `;
+      let nuevoDiv = document.createElement("div");
+      nuevoDiv.classList.add("cart-item");
+      nuevoDiv.innerHTML = `<div class="cart-item">
+      <img src="${Foto}" class = "img-fluid"> 
+      <div class="details">
+          <h5>${Nombre}</h3>
+          <button class="btn btn-danger">Quitar producto</button>
+      </div>
+      <div class="cancel"><i class="fas fa-window-close"></i></div>
+   </div>
+      `;
+      renderDom.appendChild(nuevoDiv);
     });
   } else {
     renderDom.innerHTML += `<div id ="carrito-vacio" class= "card">
@@ -94,81 +94,17 @@ const renderCart = () => {
                 `;
   }
 };
-console.log(carrito);
 
-/*
-document.addEventListener("DOMContentLoaded", () => {
-  if (localStorage.getItem("carrito")) {
-    carrito = JSON.parse(localStorage.getItem("carrito"));
-    mostrarCarrito();
-  }
+//pop up avisando la compra realizada
+avisoDeCompra.addEventListener("click", () => {
+  Swal.fire({
+    icon: "success",
+    title: "La operacion ha sido realizada!",
+    text: "Gracias por su compra!",
+  });
 });
 
-//se agrega producto al carrito
-const agregarProductoCarrito = (producto, id) => {
-  console.log(producto);
-  let productoEncontrado = catalogo.find((producto) => producto.Id === id);
-  let productoCarrito = carrito.find((producto) => producto.Id === id);
-  if (carrito.some((producto) => producto.Id === id)) {
-    const alertaBoton = document.getElementById("mybtn");
-    alertaBoton.addEventListener("click", () => {
-      Toastify({
-        text: "Producto agregado",
-        className: "info",
-        style: {
-          background: "linear-gradient(#ff3333)",
-        },
-      }).showToast();
-    });
-  } else if (productoCarrito === undefined) {
-    carrito.push({
-      Id: id,
-      Nombre: producto.querySelector("#tituloProducto").textContent,
-      Descripcion: producto.querySelector("#descripcionProducto").textContent,
-      Precio: Number(
-        producto.querySelector("#precioProducto").textContent.slice(1)
-      ),
-      Foto: producto.querySelector("#fotoProducto").getAttribute("src"),
-      Cantidad: 1,
-    });
-  } else {
-    const prodIndex = carrito.findIndex((prod) => prod.Id === Number(id));
-    carrito[prodIndex].Cantidad = carrito[prodIndex].Cantidad + 1;
-    carrito[prodIndex].Precio =
-      carrito[prodIndex].Precio + productoEncontrado.Precio;
-  }
-  console.log(carrito);
-};
-
-///muestra el carrito
-const mostrarCarrito = () => {
-  productoscarrito.classList.toggle("verCarrito");
-  updateCarrito();
-};
-
-const updateCarrito = () => {
-  let productosCarritoVista = "";
-  carrito.forEach((producto) => {
-    {
-      productosCarritoVista += `<div class="col-12 mb-2 col-md-4 col-sm-4 ">
-            <div class="card">
-            <div class="card-body">
-            <img id="fotoProducto" src="${producto.Foto}" class="card-img-top">
-            <h5 id="tituloProducto">Nombre: ${producto.Nombre}</h5>
-            <p id="descripcionProducto">Descripcion: ${producto.Descripcion}</p>
-            <p id="precioProducto">Precio: $${producto.Precio}</p>
-            <p id="precioProducto">Cantidad: ${producto.Cantidad}</p>
-            <button data-id="${producto.Id}" id="mybtnclear" name="btnBorrar" class="btn btn-danger">Borrar</button>
-            </div>
-            </div>
-            </div>
-            `;
-    }
-    document.querySelector(".carrito-main").innerHTML = productosCarritoVista;
-  });
-  localStorage.setItem("carrito", JSON.stringify(carrito));
-};
-
+/*
 productoscarrito.addEventListener("click", (e) => {
   if (e.target.classList.contains("btn-danger")) {
     borrarProductoCarrito(Number(e.target.dataset.id));
