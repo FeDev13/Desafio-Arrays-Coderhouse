e174b32f7de1e865f370521defc5496a8439a360
Federico Robledo <federicorobledo85@gmail.com>
2022-07-11 22:51:49 -0300

    localStorage mas fetch

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const productosLista = document.getElementById("contenedorProductos");
 const productoscarrito = document.querySelector(".carrito-main");
 const sumarCarrito = document.getElementById("carritonav");
+const renderDom = document.getElementById("products-id");
 
 const traerCatalogo = async () => {
   let respuesta = await fetch("./catalogo.json");
   return respuesta.json();
 };
+
+let carrito = [];
 
 const renderCards = async () => {
   let productos = await traerCatalogo();
@@ -30,6 +33,7 @@ const renderCards = async () => {
 renderCards();
 
 //evento click para agregar producto
+//evento click para agregar producto
 productosLista.addEventListener("click", (e) => {
   if (e.target.id === "mybtn") {
     guardarDatos(e.target.dataset.id);
@@ -41,9 +45,56 @@ const guardarDatos = async (id) => {
   let productoNuevo = productos.find(
     (producto) => producto.Id === parseInt(id)
   );
+  let productoEnStorage = JSON.parse(localStorage.getItem(id));
+  if (productoEnStorage === null) {
+    localStorage.setItem(id, JSON.stringify({ ...productoNuevo, Cantidad: 1 }));
+    recorrerLocalStorage();
+  } else {
+    let productoExiste = JSON.parse(localStorage.getItem(id));
+    productoExiste.Cantidad = productoExiste.Cantidad + 1;
+    productoExiste.Precio = productoExiste.Precio + productoNuevo.Precio;
+    localStorage.setItem(id, JSON.stringify(productoExiste));
+    recorrerLocalStorage();
+  }
 };
 
-let carrito = [];
+const recorrerLocalStorage = () => {
+  carrito.length = 0;
+  for (let index = 0; index < localStorage.length; index++) {
+    const element = localStorage.key(index);
+    carrito.push(JSON.parse(localStorage.getItem(element)));
+  }
+  renderCart();
+};
+
+const renderCart = () => {
+  if (carrito.length > 0) {
+    renderDom.innerHTML = "";
+    carrito.forEach((producto) => {
+      let { Id, Nombre, Foto, Categoria, Stock, Precio } = producto;
+      renderDom.innerHTML += `<div class="col-12 mb-2 col-md-4 col-sm-4 panel">
+                <div class="card panel1" style="background-color:#BF8085">
+                <div class="card-body">
+                <img id="fotoProducto" src="${Foto}" class="card-img-top">
+                <h5 id="tituloProducto">${Nombre}</h5>
+                <p id="descripcionProducto">${Categoria}</p>
+                <p id="precioProducto">$${Precio}</p>
+                <button data-id="${Id}" id="mybtn" name="btnComprar" class="btn btn-dark">Comprar</button>
+                </div>
+                </div>
+                </div>
+                `;
+    });
+  } else {
+    renderDom.innerHTML += `<div id ="carrito-vacio" class= "card">
+                <div class="card-body">
+                <h5 id="tituloProducto">${Nombre}</h5>
+                <p class = "card-text"> No hay productos</p> 
+                </div>
+                `;
+  }
+};
+console.log(carrito);
 
 /*
 document.addEventListener("DOMContentLoaded", () => {
